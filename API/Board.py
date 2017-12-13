effd0f6fdbd6613cf26e575db991a73934610d8d
Paul Coignet <crushing.of.files@gmail.com>
2017-12-13 16:04:26 +0100

    Add (Board) printConsole method

--- API/Board.py
+++ API/Board.py
@@ -4,6 +4,23 @@ class Board:
 	"""
 
 	def __init__(self, width=10, height=10, state=[' ', 'X']):
+		""" Init the class """
 		self.width = width
 		self.height = height
 		self.state = state
+
+		# Simple board
+		self.board = [[0 for _ in range(width)] for _ in range(height)]
+
+	def printConsole(self):
+		""" Print the board to the console """
+
+		print("+" + "-"*self.width + "+")
+
+		for i in range(0, self.height):
+			print('|', end='')
+			for j in range(0, self.width):
+				print(self.state[self.board[i][j]], end='')
+			print('|')
+
+		print("+" + "-"*self.width + "+")
