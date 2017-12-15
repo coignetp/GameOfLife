42f40b00b5e3674dd9efa0cfae40aae459b7f057
Paul Coignet <crushing.of.files@gmail.com>
2017-12-15 21:34:06 +0100

    Add graphic view

--- API/Board.py
+++ API/Board.py
@@ -1,16 +1,32 @@
+import pygame
+
 class Board:
 	"""
 	Board game with the different cells
 	"""
 
-	def __init__(self, width=10, height=10, state=[' ', 'X']):
+	def __init__(self, width=10, height=10, state=[' ', 'X'], window=False):
 		""" Init the class """
 		self.width = width
 		self.height = height
 		self.state = state
+		self.dimension = 2
+
+		self.cellWidth = 5
+		self.cellHeight = 5
+
+		if window:
+			self.initWindow()
 
 		# Simple board
 		self.board = [[0 for _ in range(width)] for _ in range(height)]
+
+	def update(self, f, turn=1):
+		""" Update the board of n turns """
+
+		while turn > 0:
+			self.board = f(self.board, (self.height,self.width), len(self.state))
+			turn -= 1
 
 	def printConsole(self):
 		""" Print the board to the console """
@@ -24,3 +40,25 @@ class Board:
 			print('|')
 
 		print("+" + "-"*self.width + "+")
+
+	def initWindow(self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((self.width*self.cellWidth, self.height*self.cellHeight))
+
+	def printWindow(self):
+		""" Print the board to the window
+		thanks to pygame. Only with 2D """
+
+		if self.dimension != 2:
+			return
+
+		self.screen.fill((255, 255, 255))
+
+		for i in range(0, len(self.board)):
+			for j in range(0, len(self.board[i])):
+				if self.board[i][j] == 1:
+					pygame.draw.rect(self.screen, (0, 0, 0), ((j*self.cellWidth, i*self.cellHeight), (self.cellWidth, self.cellHeight)), 0)
+
+		#pygame.display.update()
+		pygame.display.flip()
+		#clock.tick(20)
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import numpy as n
+import pygame
 from API.Board import Board
 from API.RuleConway import RuleConway
 from API.Rule import Rule
 from random import randint
 
 # Size of the board
-width = 10
-height = 10
+width = 100
+height = 100
 
 board = [[0 for _ in range(width)] for _ in range(height)]
 
@@ -16,12 +17,27 @@ for i in range(len(board)):
 		board[i][j] = randint(0,1)
 
 # Game
-b = Board(width, height)
+b = Board(width, height, [' ', 'X'], True)
 b.board = board
 r = RuleConway()
 
-for _ in range(10):
+"""for _ in range(10):
 	b.printConsole()
 	b.update(r.update)
 
-b.printConsole()
+b.printConsole()"""
+
+done = False
+
+pygame.key.set_repeat(400, 200)
+
+b.printWindow()
+
+while not done:
+	for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				done = True
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					b.update(r.update)
+					b.printWindow()
